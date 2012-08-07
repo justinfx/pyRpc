788a01a55f0a0689733647ecf2441c446f615e85
Justin Israel <justinisrael@gmail.com>
2012-08-07 14:30:19 -0700

    More interesting examples using fast and slow async/blocking calls

--- examples/client.py
+++ examples/client.py
@@ -8,20 +8,35 @@ as this client will try and communicate immediately.
 import time
 from pyRpc import RpcConnection
 
+import logging
+# logging.basicConfig(level=logging.DEBUG)
+
+ASYNC_CALLS = 0
+
+def callback(resp, *args, **kwargs):
+	global ASYNC_CALLS
+	print "Got slow response:", resp.result
+	ASYNC_CALLS += 1
 
 if __name__ == "__main__":
 
-	remote = RpcConnection("Server")
+	remote = RpcConnection("Server", workers=1)
 
 	# if the server were using a TCP connection:
 	# remote = RpcConnection("Server", tcpaddr="127.0.0.1:40000")
 
 	time.sleep(.1)
 
-	print "Calling myFunction()"
+	print "Calling slow()"
 
-	resp = remote.call("myFunction", 
-				args=(1, "a"), 
-				kwargs={'flag':True, 'option':"blarg"})
+	for i in xrange(5):	
+		remote.call("slow", async=True, callback=callback)
 
-	print resp, resp.result
+	print "Calling fast()"
+	resp = remote.call("fast")
+	print "Got fast response:", resp.result
+
+	print "Waiting on async calls to finish"
+	while ASYNC_CALLS < 5:
+		time.sleep(.1)
+
--- examples/server.py
+++ examples/server.py
@@ -3,10 +3,15 @@
 import sys, time
 import pyRpc
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
 def main():
     
     # using local IPC communication    
-    server = pyRpc.PyRpc("Server")
+    server = pyRpc.PyRpc("Server", workers=2)
 
     # Could have used a TCP connection if we wanted:
     # server = pyRpc.PyRpc("Server", tcpaddr="127.0.0.1:40000")
@@ -14,7 +19,8 @@ def main():
 
     time.sleep(.1)
 
-    server.publishService(myFunction)
+    server.publishService(slow)
+    server.publishService(fast)
     server.publishService(noReturn)
     server.start()
     
@@ -25,13 +31,27 @@ def main():
     except KeyboardInterrupt:
         server.stop()
     
+counter = 0
 
-def myFunction(*args, **kwargs):
+def slow(*args, **kwargs):
     "This does something and returns values"
-    print "myFunction() called! Doing some stuff."
-    time.sleep(2)
+
+    global counter 
+
+    print "slow() called! Doing some stuff."
+    time.sleep(3)
     print "Done!"
-    return args, kwargs
+    counter += 1
+    return counter
+
+def fast(*args, **kwargs):
+    "This does something and returns values"
+
+    global counter 
+
+    print "fast() called! Doing some stuff."
+    counter += 1
+    return counter
 
 def noReturn(value=1):
     "This does something and returns nothing"
